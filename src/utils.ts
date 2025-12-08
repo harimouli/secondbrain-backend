19eb18d73ccad0d995d896337c4cffbe251a2761
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2025-12-08 19:49:00 +0530

    feat: enhanced url gen algo using base62

--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,12 @@ import base62 from "base62";
 import crypto from "crypto";
 
 export const generateUrlHash = (len: number): string => {
-  const randomBytes = crypto.randomBytes(6).readUIntBE(0, 6);
-  let hash = base62.encode(randomBytes % 62 ** len);
+  if (!Number.isInteger(len) || len < 1 || len > 10) {
+    throw new Error("Hash length must be an integer between 1 and 10");
+  }
+
+  const randomValue = crypto.randomBytes(6).readUIntBE(0, 6);
+  let hash = base62.encode(randomValue % 62 ** len);
 
   return hash.padStart(len, "0");
 };
