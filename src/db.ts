99dee3023030ce3096ba7531753affea12035ff0
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2025-12-08 16:39:51 +0530

    feat:updated user and link schemas

--- src/db.ts
+++ src/db.ts
@@ -1,48 +1,51 @@
 import mongoose from "mongoose";
-const {Schema, model} = mongoose;
-import dotenv from "dotenv"
+const { Schema, model } = mongoose;
+import dotenv from "dotenv";
 
 dotenv.config();
 
-
-
-async function main() {
-    const mongoUrl = process.env.MONGO_URL;
-    if (!mongoUrl) {
-        throw new Error("MONGO_URL environment variable is not bhai");
-    }
-    await mongoose.connect(mongoUrl);
-    
+interface User {
+  username: string;
+  password: string;
+  activeShare?: boolean;
+  dateOfJoined?: Date;
 }
 
-export const UserSchema = new Schema({
-    username: {type: String, unique: true},
-    password: String ,
-    dateOfJoined: {
-        type:Date,
-        default: Date.now,
-    }
-  
-})
+async function main() {
+  const mongoUrl = process.env.MONGO_URL;
+  if (!mongoUrl) {
+    throw new Error("MONGO_URL environment variable is not bhai");
+  }
+  await mongoose.connect(mongoUrl);
+}
+
+export const UserSchema = new Schema<User>({
+  username: { type: String, unique: true },
+  password: String,
+  activeShare: { type: Boolean, default: false },
+  dateOfJoined: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 export const UserModel = mongoose.model("Users", UserSchema);
 
-const ContentSchema = new Schema ( {
-    title:String,
-    link: String,
-    type: String,
-    tags: [{type: mongoose.Types.ObjectId, ref: "Tag"}],
-    userId: {type: mongoose.Types.ObjectId, ref: "Users"}
-})
+const ContentSchema = new Schema({
+  title: String,
+  link: String,
+  type: String,
+  share: { type: Boolean, default: false },
+  tags: [{ type: mongoose.Types.ObjectId, ref: "Tag" }],
+  userId: { type: mongoose.Types.ObjectId, ref: "Users" },
+});
 
 export const ContentModel = mongoose.model("Content", ContentSchema);
 
-
 const LinkSchema = new Schema({
-    hash: String,
-    userId: {type: mongoose.Types.ObjectId, ref: "Users", required: true}
-
-})
+  hash: { type: String, unique: true },
+  userId: { type: mongoose.Types.ObjectId, ref: "Users", required: true },
+});
 
 export const LinkModel = mongoose.model("Links", LinkSchema);
 main();
