7e0b75c665ddaf0fb07e720b07cff54e8fa536b8
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2025-12-08 16:42:18 +0530

    feat: updated link generation algo

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,12 @@
+export const random = (len: number): string => {
+  const options: string =
+    "ejhdsmvbdfkhdfjfaqrkcfh+ghjtyhikjuyio2368695356783457890";
+  let length: number = options.length;
 
+  let randomText: string = "";
 
-
-export const random = (len: number) => {
-    const options: string = "ejhdsmvbdfkhdfjfaqrk";
-    let length: number = options.length;
-
-    let randomText: string = "";
-
-    for(let i = 0; i<len; i++){
-        randomText += options[Math.floor(Math.random() * length)] // 0 => 20
-    }
-    return randomText;
-}
+  for (let i = 0; i < len; i++) {
+    randomText += options[Math.floor(Math.random() * length)]; // 0 => 20
+  }
+  return randomText;
+};
