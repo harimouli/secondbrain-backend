b7fdf380cf5d418ad29a9974d24f58abae5fb22f
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2025-12-19 16:00:25 +0530

    feat: added email validation logic refractored

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { UserModel } from "../db";
-import { z } from "zod";
-import { JWT_SECRET, SALT_ROUNDS } from "../config";
-import { signupSchema, SignupInput } from "../validators";
+import { UserModel } from "../models/user.model";
+import { RefreshTokenModel } from "../models/refreshToken.model";
+import {
+  ACESS_TOKEN_SECRET,
+  SALT_ROUNDS,
+} from "../config/config";
+import { signupSchema } from "../validators";
+import crypto from "crypto";
 
 export const signupController = async (req: Request, res: Response) => {
   try {
@@ -12,6 +16,8 @@ export const signupController = async (req: Request, res: Response) => {
     const username: string = req.body.username;
     const password: string = req.body.password;
     const confirmPassword: string = req.body.confirmPassword;
+
+
 
     if (password !== confirmPassword) {
       res.status(400).json({
@@ -41,6 +47,7 @@ export const signupController = async (req: Request, res: Response) => {
 
     const existingUser = await UserModel.findOne({
       username,
+      email,
     });
 
     if (existingUser) {
@@ -52,6 +59,7 @@ export const signupController = async (req: Request, res: Response) => {
       return;
     }
     await UserModel.create({
+      email: email,
       username: username,
       password: hashedPassword,
     });
@@ -68,12 +76,17 @@ export const signupController = async (req: Request, res: Response) => {
   }
 };
 
+
+
+// sigin controller 
+
+
 export const signinController = async (req: Request, res: Response) => {
   try {
-    const username: string = req.body.username;
+    const email: string = req.body.email;
     const password: string = req.body.password;
 
-    if (!username || !password) {
+    if (!email || !password) {
       res.status(400).json({
         success: false,
         message: "username and password are required",
@@ -82,7 +95,7 @@ export const signinController = async (req: Request, res: Response) => {
     }
 
     const existingUser = await UserModel.findOne({
-      username,
+      email,
     });
 
     if (!existingUser) {
@@ -105,26 +118,52 @@ export const signinController = async (req: Request, res: Response) => {
       return;
     }
 
-    const token = jwt.sign(
+    const accessToken = jwt.sign(
       {
-        id: existingUser._id,
+        userId: existingUser._id,
       },
-      JWT_SECRET,
-      { expiresIn: "1d" },
+      ACESS_TOKEN_SECRET,
+      { expiresIn: "15m" }, // access token epxires in 15 mintues make sense
     );
+
+    
+  // here i am generatinf refresh token 
+    const hashedToken = crypto.randomBytes(64).toString("hex");
+    
+   // using sha256 to hash the toekn for secuity
+    const refreshToken: string = crypto.createHash("sha256").update(hashedToken).digest("hex");
+
+  // inserting refresh token in db 
+
+    await RefreshTokenModel.create({
+      token: refreshToken,
+      userId: existingUser._id,
+      expiresAt: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // refresh token expiry in 3 days
+    })
+    
+    // sending access tokens and refresh tokens in http only cookies
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      maxAge: 15 * 60 * 1000, // 15 minutes
+    });
+
+    res.cookie("refreshToken", hashedToken, {
+      httpOnly: true,
+      maxAge: 3 * 24 * 60 * 60 * 1000, // same as for token expiry , for 3 days here make sense of remmebering token expiry in mind
+    });
 
     res.status(200).json({
       success: true,
-      token: token,
       userDetails: {
         username: existingUser.username,
         dateOfJoined: existingUser.dateOfJoined,
         isShareEnabled: existingUser.isShareEnabled,
+        email: existingUser.email,
       },
-
       message: "You are logged in!",
     });
-  } catch {
+  } catch(err) {
+    console.error("Error during signin:", err);
     res.status(500).json({
       success: false,
       message: "something went wrong!",
