73c0cc14cbc279166754981f337811a7c68dc652
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2025-12-19 17:04:24 +0530

    feat: added global rate limiting

--- src/middleware/authRateLimiter.ts
+++ src/middleware/authRateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 import { Request, Response } from "express";
 export const authRateLimiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes..lol protection from bad guys trying to brute force login.. lol
-  max: 8, // limit each IP to 8 requests per windowMs....lol brute force protection
+  max: 8, // limit each IP to 8 requests per windowMs....lol brute force protection 
   handler: (req: Request, res: Response) => {
     res.status(429).json({
       success: false,
