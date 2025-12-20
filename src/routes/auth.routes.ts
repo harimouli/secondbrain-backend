34aaefb77fe9884c91abf0df8eb67ede63518540
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2025-12-21 05:12:26 +0530

    feat: verifylogin end point

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,10 +4,11 @@ import {
   signinController,
 } from "../controllers/auth.controller";
 
+import { verifyauth } from "../controllers/verifyauth.controller";
 const authRouter = Router();
 
 authRouter.post("/signup", signupController);
-
 authRouter.post("/signin", signinController);
+authRouter.get("/verifylogin", verifyauth);
 
 export default authRouter;
