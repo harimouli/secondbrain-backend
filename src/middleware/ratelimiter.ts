019c46d70e180d9552fc38c92b2692e7fd35e31c
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2026-01-23 11:21:38 +0530

    fix: fixed reqs for window

--- src/middleware/ratelimiter.ts
+++ src/middleware/ratelimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 import { Request, Response } from "express";
 export const requestRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes line up with common api rate limiting
+  windowMs: 20 * 60 * 1000, // 15 minutes line up with common api rate limiting
   max: 100, // limited  each IP to 100 requests per windowMs
 
   handler: (req: Request, res: Response) => {
